79d88932a561b613d12597c7135fca69cf1f01c4
92316 <92316@farnborough.ac.uk>
2020-09-29 12:03:12 +0100

    -tested track/delete - PASSED -Created track/get -tested track/get - FAILED (logic errors - still need to debug)

--- src/main/java/controllers/Track.java
+++ src/main/java/controllers/Track.java
@@ -1,19 +1,22 @@
 package controllers;
 
+import org.json.simple.JSONObject;
 import server.Main;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-@Path("Tracks/")
+@Path("tracks/")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.APPLICATION_JSON)
 
-public class Tracks {
+public class Track {
 
     @POST
     @Path("delete/{TrackID}")
+
 
     public String trackDelete(@PathParam("TrackID") Integer TrackID){
         System.out.println("Invoked trackDelete()");
@@ -21,11 +24,33 @@ public class Tracks {
             PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Tracks WHERE TrackID = ?");
             ps.setInt(1, TrackID);
             ps.execute();
-            return "{\"OK\": \"Track deleted\"}";
+            return "{\"OK\": \"Tracks deleted\"}";
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
             return "{\"Error\": \"Unable to delete item, please see server console for more info.\"}";
         }
     }
+    @GET
+    @Path("get/{TrackID}")
+
+    public String trackGet(@PathParam("TrackID") Integer TrackID){
+        System.out.println("Invoked Tracks.trackGet() with TrackID " + TrackID);
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT Address FROM Tracks WHERE TrackID = ?");
+            ps.setInt(1, TrackID);
+            ResultSet results = ps.executeQuery();
+            JSONObject response = new JSONObject();
+            if (results.next()== true) {
+                response.put("Track", TrackID);
+                response.put("Address", results.getString(1));
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
 }
+
 
